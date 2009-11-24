4a7c8b5e52eeff675f9bd0bd875eedc35387e97f
pdwhite <pdwhite@19c0c642-d4e6-44e4-92e5-4f3623b665b3>
2009-11-24 20:49:32 +0000

    Added mod operator template tag. Got VLAN subscription edit page done.
    git-svn-id: https://jsl8/svn@357 19c0c642-d4e6-44e4-92e5-4f3623b665b3

--- common/templatetags/comparisons.py
+++ common/templatetags/comparisons.py
@@ -1,44 +1,47 @@
 from django.template import Library
+register = Library()
 
+@register.filter
+def mod(value, arg):
+    "Returns the modulo of the args"
+    return value % arg
+
+@register.filter
 def gt(value, arg):
     "Returns a boolean of whether the value is greater than the argument"
     return value > int(arg)
 
+@register.filter
 def lt(value, arg):
     "Returns a boolean of whether the value is less than the argument"
     return value < int(arg)
 
+@register.filter
 def gte(value, arg):
     "Returns a boolean of whether the value is greater than or equal to the argument"
     return value >= int(arg)
 
+@register.filter
 def lte(value, arg):
     "Returns a boolean of whether the value is less than or equal to the argument"
     return value <= int(arg)
 
+@register.filter
 def length_gt(value, arg):
     "Returns a boolean of whether the value's length is greater than the argument"
     return len(value) > int(arg)
 
+@register.filter
 def length_lt(value, arg):
     "Returns a boolean of whether the value's length is less than the argument"
     return len(value) < int(arg)
 
+@register.filter
 def length_gte(value, arg):
     "Returns a boolean of whether the value's length is greater than or equal to the argument"
     return len(value) >= int(arg)
 
+@register.filter
 def length_lte(value, arg):
     "Returns a boolean of whether the value's length is less than or equal to the argument"
     return len(value) <= int(arg)
-
-register = Library()
-register.filter('gt', gt)
-register.filter('lt', lt)
-register.filter('gte', gte)
-register.filter('lte', lte)
-register.filter('length_gt', length_gt)
-register.filter('length_lt', length_lt)
-register.filter('length_gte', length_gte)
-register.filter('length_lte', length_lte)
-
